0e663beaa142dc666ee2aa3e9c5e235029b92248
nils <nilseckerle2001@gmail.com>
2023-12-25 14:50:58 +0000

    remove old configs

--- emacs/.emacs.d/elpa/emacsql-sqlite-module-20230224.1201/emacsql-sqlite-module-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from emacsql-sqlite-module.el  -*- no-byte-compile: t -*-
-(define-package "emacsql-sqlite-module" "20230224.1201" "EmacSQL back-end for SQLite using a module" '((emacs "25") (emacsql "20230220") (sqlite3 "0.16")) :commit "7c533fb6c27c3a10b6ab05bddf663e37c109e459" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/emacsql")
