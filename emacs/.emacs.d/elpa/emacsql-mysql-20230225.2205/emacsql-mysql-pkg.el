fd4c774212dd07ad1139cb24bb125af9c5811821
nils <nilseckerle2001@gmail.com>
2023-12-25 14:50:58 +0000

    remove old configs

--- emacs/.emacs.d/elpa/emacsql-mysql-20230225.2205/emacsql-mysql-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from emacsql-mysql.el  -*- no-byte-compile: t -*-
-(define-package "emacsql-mysql" "20230225.2205" "EmacSQL back-end for MySQL" '((emacs "25.1") (emacsql "20230220")) :commit "b436adf09ebe058c28e0f473bed90ccd7084f6aa" :authors '(("Christopher Wellons" . "wellons@nullprogram.com")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/emacsql")
