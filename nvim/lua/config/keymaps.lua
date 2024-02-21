55b64076ac88e5a0828a7edc3ae6d94eacae4412
Nils <nilseckerle2001@gmail.com>
2024-02-21 20:10:53 +0100

    added movement. A L to move to beginn and end of line

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
---Move Line up/down
+-- Move Line up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- movement
+vim.keymap.set("n", "H", "0")
+vim.keymap.set("n", "L", "$")
