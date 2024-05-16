fe526db8d7dc3475b90b23b3418e0e246e30bb0f
nils <nilseckerle2001@gmail.com>
2024-05-16 15:55:08 +0200

    added nvim-cmp & luasnip configuration

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,141 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    -- Not all LSP servers add brackets when completing a function.
+    -- To better deal with this, LazyVim adds a custom option to cmp,
+    -- that you can configure. For example:
+    --
+    -- ```lua
+    -- opts = {
+    --   auto_brackets = { "python" }
+    -- }
+    -- ```
+
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+      return {
+        auto_brackets = {}, -- configure any filetype to auto add brackets
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("lazyvim.config").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+    ---@param opts cmp.ConfigSchema | {auto_brackets?: string[]}
+    config = function(_, opts)
+      for _, source in ipairs(opts.sources) do
+        source.group_index = source.group_index or 1
+      end
+      local cmp = require("cmp")
+      local Kind = cmp.lsp.CompletionItemKind
+      cmp.setup(opts)
+      cmp.event:on("confirm_done", function(event)
+        if not vim.tbl_contains(opts.auto_brackets or {}, vim.bo.filetype) then
+          return
+        end
+        local entry = event.entry
+        local item = entry:get_completion_item()
+        if vim.tbl_contains({ Kind.Function, Kind.Method }, item.kind) and item.insertTextFormat ~= 2 then
+          local cursor = vim.api.nvim_win_get_cursor(0)
+          local prev_char = vim.api.nvim_buf_get_text(0, cursor[1] - 1, cursor[2], cursor[1] - 1, cursor[2] + 1, {})[1]
+          if prev_char ~= "(" and prev_char ~= ")" then
+            local keys = vim.api.nvim_replace_termcodes("()<left>", false, false, true)
+            vim.api.nvim_feedkeys(keys, "i", true)
+          end
+        end
+      end)
+    end,
+  }, -- {
+  --   "hrsh7th/nvim-cmp",
+  --   lazy = false,
+  --   priority = 100,
+  --   dependencies = {
+  --     "onsails/lspkind.nvim",
+  --     "hrsh7th/cmp-nvim-lsp",
+  --     "hrsh7th/cmp-path",
+  --     "hrsh7th/cmp-buffer",
+  --     { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+  --   },
+  --   config = function()
+  --     vim.opt.completeopt = { "menu", "menuone", "noselect" }
+  --     vim.opt.shortmess:append("c")
+  --
+  --     local lspkind = require("lspkind")
+  --     lspkind.init({})
+  --
+  --     local cmp = require("cmp")
+  --
+  --     cmp.setup({
+  --       sources = {
+  --         { name = "nvim_lsp" },
+  --         { name = "path" },
+  --         { name = "buffer" },
+  --       },
+  --       mapping = {
+  --         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-b>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-y>"] = cmp.mapping(
+  --           cmp.mapping.confirm({
+  --             behavior = cmp.ConfirmBehavior.Insert,
+  --             select = true,
+  --           }),
+  --           { "i", "c" }
+  --         ),
+  --       },
+  --
+  --       -- Enable luasnip to handle snippet expansion for nvim-cmp
+  --       snippet = {
+  --         expand = function(args)
+  --           require("luasnip").lsp_expand(args.body)
+  --         end,
+  --       },
+  --     })
+  --   end,
+  -- },
+}
