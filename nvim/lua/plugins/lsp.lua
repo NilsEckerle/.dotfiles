91c368ad7d3f63d305f38c53bc05248022f0cb25
N1lsE <nilseckerle2001@gmail.com>
2024-12-08 22:37:53 +0100

    added lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,19 @@
 return {
-    {'neovim/nvim-lspconfig'},
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" }}
+                },
+            },
+        },
+        config =function()
+            require("lspconfig").lua_ls.setup {}
+        end
+    },
     {'hrsh7th/cmp-nvim-lsp'},
     {
         'hrsh7th/nvim-cmp',
