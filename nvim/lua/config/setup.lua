28b651aaa975dfaa08c1ac75dd380abf58f422e5
N1lsE <nilseckerle2001@gmail.com>
2024-12-10 16:25:24 +0100

    changed cursor to Block

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -4,6 +4,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.colorcolumn = "80"
+vim.opt.guicursor = "n-v-i-c:block"
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.signcolumn = 'no'
