e838971bd5a234b32bdf36a85972531cb482ffc1
N1lsE <nilseckerle2001@gmail.com>
2024-12-24 23:11:42 +0100

    added formatting

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'stevearc/conform.nvim',
+        opts = {},
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    python = { "isort", "black" },
+                    rust = { "rustfmt", lsp_format = "fallback" },
+                    javascript = { "prettierd", "prettier", stop_after_first = true },
+                },
+            })
+        end
+    }
+}
