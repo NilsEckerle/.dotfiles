3e9e52141a69604ba26ee25ec69022e70b9d6ced
N1lsE <nilseckerle2001@gmail.com>
2024-12-24 23:12:10 +0100

    added undofile and removed/moved some things

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -5,11 +5,15 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.colorcolumn = "80"
 vim.opt.guicursor = "n-v-i-c:block"
+vim.opt.scrolloff = 5
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.signcolumn = 'no'
 
 vim.opt.ignorecase = true
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath('data') .. '/undo'
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight wen yanking (copying) text",
@@ -19,44 +23,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-local lspconfig_defaults = require('lspconfig').util.default_config
-lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end,
-})
-
-require('lspconfig').pyright.setup({})
-require('lspconfig').lua_ls.setup({})
-
-local cmp = require('cmp')
-
-cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({}),
+-- disable popup when changing config
+require('lazy').setup({
+    change_detection = {
+        enabled = false,
+        notify = false,
+    }
 })
