6d59e3ff1409dd506d989a64fb8a13afa8c75e95
N1lsE <nilseckerle2001@gmail.com>
2025-01-01 16:09:16 +0100

    some completion config

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -24,6 +24,27 @@ return {
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
             },
+            completion = {
+                accept = {
+                    auto_brackets = {
+                        enabled = false,
+                    }
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 0
+                }
+            },
+            snippets = {
+                expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+                active = function(filter)
+                    if filter and filter.direction then
+                        return require('luasnip').jumpable(filter.direction)
+                    end
+                    return require('luasnip').in_snippet()
+                end,
+                jump = function(direction) require('luasnip').jump(direction) end,
+            },
         },
         opts_extend = { "sources.default" }
     },
