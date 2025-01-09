e14fea825aec71834f7aed926d6d59ad0b5669ff
N1lsE <nilseckerle2001@gmail.com>
2025-01-10 00:00:05 +0100

    style(nvim): completion.lua something but not realy anything.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,51 +1,55 @@
 return {
-    { 'hrsh7th/nvim-cmp' },
+	{ "hrsh7th/nvim-cmp" },
 
-    { 'hrsh7th/cmp-nvim-lsp' },
+	{ "hrsh7th/cmp-nvim-lsp" },
 
-    {
-        'saghen/blink.cmp',
-        dependencies = { -- provides snippets for the snippet source
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-        version = '*',
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
-        opts = {
-            keymap = { preset = 'default' },
-            appearance = {
-                use_nvim_cmp_as_default = false,
-                nerd_font_variant = 'mono'
-            },
-            sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
-            },
-            completion = {
-                accept = {
-                    auto_brackets = {
-                        enabled = false,
-                    }
-                },
-                documentation = {
-                    auto_show = true,
-                    auto_show_delay_ms = 0
-                }
-            },
-            snippets = {
-                expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-                active = function(filter)
-                    if filter and filter.direction then
-                        return require('luasnip').jumpable(filter.direction)
-                    end
-                    return require('luasnip').in_snippet()
-                end,
-                jump = function(direction) require('luasnip').jump(direction) end,
-            },
-        },
-        opts_extend = { "sources.default" }
-    },
+	{
+		"saghen/blink.cmp",
+		dependencies = { -- provides snippets for the snippet source
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		version = "*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "default" },
+			appearance = {
+				use_nvim_cmp_as_default = false,
+				nerd_font_variant = "mono",
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			completion = {
+				accept = {
+					auto_brackets = {
+						enabled = false,
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 0,
+				},
+			},
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
 }
