5690dcb833467269986b46a8ad57ca82f479ac62
N1lsE <nilseckerle2001@gmail.com>
2025-01-29 15:00:07 +0100

    feat(nvim): some formatting changes

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,7 +19,12 @@ return {
 		config = function()
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-			require("lspconfig").pyright.setup({ capabilities = capabilities })
+			require("lspconfig").pyright.setup({ 
+				capabilities = capabilities,
+				on_attach = function (client, bufnr)
+					client.server_capabilities.documentFormattingProvider = true
+				end
+			})
 			require("lspconfig").clangd.setup({
 				cmd = { "clangd" },
 				filetypes = { "c", "cpp", "objc", "objcpp" },
