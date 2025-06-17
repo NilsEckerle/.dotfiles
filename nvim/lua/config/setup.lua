4b795067445f84a8781c85ad4d98b3bb80d6340f
Nils-WZ-dualboot-mint <nilseckerle2001@protonmail.com>
2025-06-17 22:03:58 +0200

    fix

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -31,6 +31,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- TTY-specific configuration
+if os.getenv("TERM") == "linux" then
+    vim.opt.termguicolors = false
+    vim.opt.number = true
+    vim.opt.relativenumber = false  -- Relative numbers can be problematic in TTY
+    vim.cmd([[
+        set t_Co=16
+        " Force line number colors
+        highlight LineNr ctermfg=8 ctermbg=NONE
+        highlight CursorLineNr ctermfg=11 ctermbg=NONE
+    ]])
+else
+    vim.opt.termguicolors = true
+    vim.opt.number = true
+    vim.opt.relativenumber = true
+end
+
 -- disable popup when changing config
 require("lazy").setup({
 	change_detection = {
@@ -38,21 +55,3 @@ require("lazy").setup({
 		notify = false,
 	},
 })
-
--- TTY-specific configuration
-if os.getenv("TERM") == "linux" then
-    vim.opt.termguicolors = false
-    vim.cmd([[
-        set t_Co=16
-        colorscheme default
-        " Or use a 16-color compatible theme
-        " colorscheme gruvbox
-        " set background=dark
-    ]])
-else
-    vim.opt.termguicolors = true
-    vim.cmd([[
-        colorscheme gruvbox
-        set background=dark
-    ]])
-end
