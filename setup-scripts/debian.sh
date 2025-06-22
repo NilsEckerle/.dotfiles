73f9a58a6cc470bcf881e286ada651172aa2002a
nils <nils@okabe.future-gadget-lab.com>
2025-06-22 22:39:58 +0200

    removed env for rofi. hardcoded it in i3

--- setup-scripts/debian.sh
+++ setup-scripts/debian.sh
@@ -684,7 +684,6 @@ create_symlinks() {
         "scripts:$HOME/scripts"
         "rofi:$HOME/.config/rofi"
         ".xprofile:$HOME/.xprofile"
-        "environment.d:$HOME/.config/environment.d"
     )
 
     # Add i3 config if i3 is being installed
