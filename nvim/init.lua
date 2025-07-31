7ef306094586f2df530caa65266e4032fda91b4b
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-07-31 22:36:43 +0200

    Refactor Neovim configuration structure
    - Add LazyVim global to Lua diagnostics
    - Remove old config/init.lua in favor of modular approach
    - Update init.lua to use new config structure with lazy loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
-require("config.init")
 require("config.setup")
+require("config.lazy") -- also includes plugins
 require("config.keymap")
+require("config.after")
 vim.g.lazy_disable_warn_reload = true -- Suppress the re-sourcing warning
