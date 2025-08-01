80e58924fbcba4e3c863ffbf54c786e7730c41fb
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-08-01 14:41:43 +0200

    fix(nvim): remove duplicate telescope-ui-select dependency
    Remove redundant telescope-ui-select plugin entry in telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -28,8 +28,6 @@ return {
 			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
 
-			{ "nvim-telescope/telescope-ui-select.nvim" },
-
 			-- Useful for getting pretty icons, but requires a Nerd Font.
 			{ "nvim-tree/nvim-web-devicons", enabled = true },
 		},
