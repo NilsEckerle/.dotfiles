33b9e165445624949d0a2b0b35cb43338b4bee0d
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-08-09 21:40:29 +0200

    refactor(nvim): simplify cmp config and remove LuaSnip

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,139 +1,91 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		event = "VeryLazy",
-		dependencies = {
-			-- Snippet engine
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+  {
+    "hrsh7th/nvim-cmp",
+    event = "VeryLazy",
+    dependencies = {
+      -- Completion sources
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    config = function()
+      local cmp = require("cmp")
 
-			-- Completion sources
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
+      cmp.setup({
+        -- Configure completion behavior
+        completion = {
+          completeopt = "menu,menuone,noinsert", -- Show menu, select first item, but don't insert
+        },
+        -- Configure preselect behavior
+        preselect = cmp.PreselectMode.Item, -- Preselect first item
+        -- Your preferred keybindings
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          -- ["<C-Space>"] = cmp.mapping.complete(),
+          -- ["<C-e>"] = cmp.mapping.abort(),
+          -- ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          -- ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        }),
 
-			-- More explicit snippet loading
-			local friendly_path = vim.fn.stdpath("data") .. "/lazy/friendly-snippets"
-			require("luasnip.loaders.from_vscode").lazy_load({
-				paths = { friendly_path },
-				include = nil, -- Load all languages
-				exclude = {},
-			})
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
 
-			-- Also try the standard load
-			require("luasnip.loaders.from_vscode").load({
-				paths = { friendly_path .. "/snippets" },
-			})
+        formatting = {
+          format = function(entry, vim_item)
+            -- Show source name
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
 
-			luasnip.filetype_extend("tex", { "all" })
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+      })
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
+      -- Command line completion with same behavior
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline({
+          ["<C-n>"] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }) },
+          ["<C-p>"] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }) },
+          ["<C-y>"] = { c = cmp.mapping.confirm({ select = true }) },
+        }),
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
 
-				-- Configure completion behavior
-				completion = {
-					completeopt = "menu,menuone,noinsert", -- Show menu, select first item, but don't insert
-				},
-
-				-- Configure preselect behavior
-				preselect = cmp.PreselectMode.Item, -- Preselect first item
-
-				-- Your preferred keybindings
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-
-					-- Tab for snippet navigation
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-
-				sources = cmp.config.sources({
-					{ name = "luasnip" },
-					{ name = "nvim_lsp" },
-					{ name = "path" },
-				}, {
-					{ name = "buffer" },
-				}),
-
-				formatting = {
-					format = function(entry, vim_item)
-						-- Show source name
-						vim_item.menu = ({
-							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
-							buffer = "[Buffer]",
-							path = "[Path]",
-						})[entry.source.name]
-						return vim_item
-					end,
-				},
-
-				window = {
-					-- completion = cmp.config.window.bordered(),
-					-- documentation = cmp.config.window.bordered(),
-				},
-			})
-
-			-- Command line completion with same behavior
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline({
-					["<C-n>"] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }) },
-					["<C-p>"] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }) },
-					["<C-y>"] = { c = cmp.mapping.confirm({ select = true }) },
-				}),
-				completion = {
-					completeopt = "menu,menuone,noinsert",
-				},
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-			})
-
-			-- Search completion with same behavior
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline({
-					["<C-n>"] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }) },
-					["<C-p>"] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }) },
-					["<C-y>"] = { c = cmp.mapping.confirm({ select = true }) },
-				}),
-				completion = {
-					completeopt = "menu,menuone,noinsert",
-				},
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-		end,
-	},
+      -- Search completion with same behavior
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline({
+          ["<C-n>"] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }) },
+          ["<C-p>"] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }) },
+          ["<C-y>"] = { c = cmp.mapping.confirm({ select = true }) },
+        }),
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        sources = {
+          { name = "buffer" },
+        },
+      })
+    end,
+  },
 }
