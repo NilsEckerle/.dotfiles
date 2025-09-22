fca164e70ce75235602be0027ad1fc1f8ffe53a4
Nils Eckerle <=>
2025-09-22 21:33:55 +0200

    .

--- install-scripts/000-dependencies.sh
+++ install-scripts/000-dependencies.sh
@@ -50,7 +50,7 @@ install_homebrew() {
   export NONINTERACTIVE=1
 
   # Install Homebrew with automatic yes responses
-  /bin/bash -c "$(wget -Os- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
+  yes  | /bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
 
   # Add Homebrew to PATH for current session
   if [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
