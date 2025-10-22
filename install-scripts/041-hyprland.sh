dc6c226e77eae1d9c9bff4b9fc1cee37f53599bb
Nils Eckerle <=>
2025-10-22 17:31:29 +0200

    add screenshot and colorpicker to hyprland config

--- install-scripts/041-hyprland.sh
+++ install-scripts/041-hyprland.sh
@@ -36,6 +36,10 @@ APT_PACKAGES=(
     wl-clipboard
     swaylock
     hyprpaper
+    papirus-icon-theme
+    hyprpicker
+    grim
+    slurp
   )
 
 BREW_PACKAGES=()
@@ -47,6 +51,58 @@ SYMLINKS=(
     "wallpaper:$HOME/wallpaper"
     "wofi:$HOME/.config/wofi"
   )
+
+create_color_picker_desktop_file() {
+  local desktop_file="$HOME/.local/share/applications/hyprpicker.desktop"
+
+  # Create directory if it doesn't exist
+  mkdir -p "$HOME/.local/share/applications"
+
+  log_info "Creating Hyprpicker desktop file at $desktop_file"
+
+  # Create the desktop file
+  echo "[Desktop Entry]
+Type=Application
+Name=Hyprpicker
+Comment=Wayland color picker for Hyprland
+Exec=hyprpicker -a
+Icon=color-select
+Terminal=false
+Categories=Utility;Graphics;
+Keywords=color;picker;select;copy;
+StartupNotify=false" > "$desktop_file"
+
+  # Make it executable
+  chmod +x "$desktop_file"
+
+  log_info "Hyprpicker desktop file created successfully."
+}
+
+create_grim_slurp_screenshot_desktop_file() {
+  local desktop_file="$HOME/.local/share/applications/grim_slurp_screenshot.desktop"
+
+  # Create directory if it doesn't exist
+  mkdir -p "$HOME/.local/share/applications"
+
+  log_info "Creating grim slurp screenshot desktop file at $desktop_file"
+
+  # Create the desktop file
+  echo "[Desktop Entry]
+Type=Application
+Name=WScreenshot
+Comment=Wayland screenshot tool for Hyprland
+Exec=$(HOME)/scripts/wayland-screenshot.sh
+Icon=/usr/share/icons/Papirus/32x32/apps/accessories-screenshot.svg
+Terminal=false
+Categories=Utility;Graphics;
+Keywords=screenshot;picker;select;copy;
+StartupNotify=false" > "$desktop_file"
+
+  # Make it executable
+  chmod +x "$desktop_file"
+
+  log_info "Hyprpicker desktop file created successfully."
+}
 
 # Function to install packages via apt
 install_apt_packages() {
@@ -196,6 +252,7 @@ main() {
   install_apt_packages
   install_brew_packages
   create_symlinks
+  create_color_picker_desktop_file
 }
 
 main
