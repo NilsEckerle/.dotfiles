bf731499c1aa14975b385aebfbe8dd8430554618
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-11-25 13:58:36 +0100

    feat(scripts): add Zotero backup automation

---
+++ scripts/backup_zotero.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+DISPLAY=:0
+cd ~/Zotero && {
+  git add . &&
+  (git diff --cached --quiet ||
+  git commit -m "Backup_$(date +%Y-%m-%d_%H-%M)") &&
+  git pull --rebase 2>/tmp/git_error &&
+  git push 2>/tmp/git_error
+} || { 
+  ERROR=$(cat /tmp/git_error 2>/dev/null || echo "Unknown git error");
+  notify-send -u critical -t 0 -a "Zotero" "Git failed: $ERROR";
+}
