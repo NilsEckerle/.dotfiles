251ab781303d193a9dedf2cf5c4e28671ea37978
Nils Eckerle <nilseckerle2001@protonmail.com>
2025-12-27 11:41:52 +0100

    feat(lsp): enhance clangd with tidy & placeholders

--- nvim/lua/plugins/lsp/clangd.lua
+++ nvim/lua/plugins/lsp/clangd.lua
@@ -1,6 +1,14 @@
 return function(capabilities)
 	vim.lsp.config.clangd = {
-		cmd = { "clangd" },
+		cmd = {
+			"clangd",
+			"--background-index",
+			"--clang-tidy",
+			"--header-insertion=iwyu",
+			"--completion-style=detailed",
+			"--function-arg-placeholders",
+			"--fallback-style=llvm",
+		},
 		filetypes = { "c", "cpp", "objc", "objcpp" },
 		root_markers = {
 			".clangd",
@@ -9,12 +17,16 @@ return function(capabilities)
 			"_clang-format",
 			"compile_commands.json",
 			"compile_flags.txt",
-			"build"
+			"build",
+		},
+		init_options = {
+			usePlaceholders = true,
+			completeUnimported = true,
+			clangdFileStatus = true,
 		},
 		capabilities = capabilities,
 		on_attach = function(client, bufnr)
-      -- fix
-      client.server_capabilities.signatureHelpProvider = false
+			-- client.server_capabilities.signatureHelpProvider = false
 			-- Enable inlay hints if supported
 			if client.server_capabilities.inlayHintProvider then
 				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
