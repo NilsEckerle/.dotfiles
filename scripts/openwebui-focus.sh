506d174e24dbcfb3fe8fa9181790110e6ac83291
Nils Eckerle <nilseckerle2001@protonmail.com>
2026-08-03 19:10:47 +0200

    fix(scripts): update openwebui port to 3000

--- scripts/openwebui-focus.sh
+++ scripts/openwebui-focus.sh
@@ -6,7 +6,7 @@ OPENWEBUI_CLASS="chrome-localhost__-openwebui"
 CLIENT=$(hyprctl clients -j | jq -r ".[] | select(.class == \"$OPENWEBUI_CLASS\") | .address" | head -n1)
 
 if [ -z "$CLIENT" ]; then
-    chromium --app=http://localhost:8080/ --class=openwebui --profile-directory=openwebui &
+    chromium --app=http://localhost:3000/ --class=openwebui --profile-directory=openwebui &
 else
     CURRENT_WS=$(hyprctl activeworkspace -j | jq -r '.id')
     hyprctl dispatch movetoworkspacesilent "$CURRENT_WS,address:$CLIENT"
